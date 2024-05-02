fad61d63305bc8aeecca7cd6308f2539981f34d8
SammMartins <sammuel.martins+github@ftc.edu.br>
2024-05-02 16:18:49 -0300

    Update

---
+++ Consultas/Comerciais/4Dashboards/vendasRealTime_dashboard/vendaPorFornecedor.sql
@@ -0,0 +1,38 @@
+WITH Fat AS 
+     (SELECT distinct(a.codusur) codusur,
+             SUM(a.vlatend) Valor
+        FROM pontual.PCPEDC a
+        JOIN pontual.pcclient c on c.codcli = a.codcli
+        WHERE a.DATA BETWEEN '{dtIni}' AND '{dtFim}'
+          AND a.DTCANCEL IS NULL
+          AND a.CONDVENDA IN (1, 2, 3, 7, 9, 14, 15, 17, 18, 19, 98)
+          AND a.CODCOB IN ('7563','CH','C','D','DH')
+          AND a.vlbonific = 0
+          --AND c.TIPOFJ = 'J' 
+          GROUP BY a.codusur),
+-----------------------------------------------------------------------------------------------------------------------
+DN AS (SELECT ped.codusur codusur,
+                COUNT(DISTINCT ped.codcli) AS DN
+            FROM pontual.PCPEDC ped
+                JOIN pontual.PCPEDI pedi on pedi.numped = ped.numped
+                JOIN pontual.pcprodut prod on pedi.codprod = prod.codprod
+            WHERE ped.DATA BETWEEN '{dtIni}' AND '{dtFim}'
+                AND PED.CONDVENDA IN (1, 2, 3, 7, 9, 14, 15, 17, 18, 19, 98)
+            GROUP BY ped.codusur),
+-----------------------------------------------------------------------------------------------------------------------
+BASE AS (SELECT c.CODUSUR1, COUNT(DISTINCT(c.CODCLI)) BASE
+         FROM pontual.pcclient c
+         JOIN pontual.pcusuari u on c.CODUSUR1 = u.CODUSUR
+         WHERE u.nome LIKE 'PMU%'
+         GROUP BY c.CODUSUR1
+         ORDER BY c.CODUSUR1)
+-----------------------------------------------------------------------------------------------------------------------
+SELECT U.codsupervisor, f.codusur || ' - ' || 
+       SUBSTR(U.nome, INSTR(U.nome, ' ') + 1, INSTR(U.nome, ' ', INSTR(U.nome, ' ') + 1) - INSTR(U.nome, ' ') - 1) AS RCA,
+       NVL(f.valor,0), NVL(D.DN,0), b.base
+FROM FAT F
+JOIN DN D on d.codusur = f.codusur
+JOIN pontual.pcusuari u on u.codusur = f.codusur
+JOIN BASE B on b.CODUSUR1= f.codusur
+WHERE f.codusur not in (10,50)
+ORDER BY  f.valor desc
